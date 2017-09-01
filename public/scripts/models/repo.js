45349945c25350b5a560e34c138ac455de13a266
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-08-31 21:14:50 -0700

    server i think

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -12,9 +12,8 @@ var app = app || {};
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
     $.ajax({
-      url: `https://api.github.com/user/repos`,
+      url: `/github/*`,
       type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
     })
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
